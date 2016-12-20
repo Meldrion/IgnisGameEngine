b01284e534b21d5526fd0fd29236d92f01c5d693
Fabien Steines <fabien.steines@gmail.com>
2016-12-20 12:37:47 +0100

    update - AssetStructure

--- ignis/project/AssetStructure.js
+++ ignis/project/AssetStructure.js
@@ -1,22 +1,131 @@
+"use strict";
+
+
+var convertPath = require("../engine/FileSystem.js");
+
+/**
+ *
+ */
 class AssetStructure {
 
-    static   ASSET = "asset";
-    static   ANIMATION = "animation";
-    static   BACKGROUNDMUSIC = "bgm";
-    static   BATTLEBACKGROUND = "battlebackground";
-    static   BATTLESPRITE = "battlesprite";
-    static   CHARACTER = "character";
-    static   JSON = "json";
-    static   OVERLAY = "overlay";
-    static   MAP = "map";
-    static   GAMEOVER = "gameover";
-    static   SCENEBACKGROUND = "scenebackground";
-    static   SCRIPT = "script";
-    static   SOUNDEFFECT = "soundeffect";
-    static   TILESET = "tileset";
-    static   TERRAIN = "terrain";
-    static   TITLE = "title";
-    static   UI = "ui";
+    /**
+     * @return {string}
+     */
+    static get ASSET() {
+        return "asset"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get ANIMATION() {
+        return "animation"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get BACKGROUNDMUSIC() {
+        return "bgm"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get BATTLEBACKGROUND() {
+        return "battlebackground"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get BATTLESPRITE() {
+        return "battlesprite"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get CHARACTER() {
+        return "character"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get JSON() {
+        return "json"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get OVERLAY() {
+        return "overlay"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get MAP() {
+        return "map"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get GAMEOVER() {
+        return "gameover"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get SCENEBACKGROUND() {
+        return "scenebackground"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get SCRIPT() {
+        return "script"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get SOUNDEFFECT() {
+        return "soundeffect"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get TILESET() {
+        return "tileset"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get TERRAIN() {
+        return "terrain"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get TITLE() {
+        return "title"
+    };
+
+    /**
+     * @return {string}
+     */
+    static get UI() {
+        return "ui"
+    };
 
     /**
      *
@@ -134,7 +243,7 @@ class AssetStructure {
      * @returns {Array.<T>|Buffer|string}
      */
     getAsset() {
-        return FilesystemHandler.concat(this.rootPath, AssetStructure.ASSET);
+        return convertPath(this.rootPath, AssetStructure.ASSET);
     }
 
     /**
@@ -143,7 +252,7 @@ class AssetStructure {
      * @returns {Array.<T>|Buffer|string}
      */
     getPath(assetName) {
-        return FilesystemHandler.concat(this.getAsset(), assetName != null ? assetName.toLowerCase() : "not valid");
+        return convertPath(this.getAsset(), assetName != null ? assetName.toLowerCase() : "not valid");
     }
 
     /**
@@ -151,8 +260,9 @@ class AssetStructure {
      * @returns {Array.<T>|Buffer|string}
      */
     getProjectJSON() {
-        return FilesystemHandler.concat(this.rootPath, "project.json");
+        return convertPath(this.rootPath, "project.json");
     }
 
-
 }
+
+module.exports = AssetStructure;
--- ignis/project/IgnisGame.js
+++ ignis/project/IgnisGame.js
@@ -1,12 +1,24 @@
+"use strict";
 
 var MapManager = require("./map/MapManager");
 var TilesetManager = require("./map/TilesetManager");
+var AssetStructure = require("./AssetStructure.js");
 
+/**
+ *
+ */
 class IgnisGame {
 
     constructor(basePath) {
-        this.mapManager = new MapManager("","");
-        this.tilesetManager = new TilesetManager("");
+        this.assetStructure = new AssetStructure(basePath);
+        this.mapManager = new MapManager(this.assetStructure.getPath(AssetStructure.MAP),
+            this.assetStructure.getPath(AssetStructure.JSON));
+        this.tilesetManager = new TilesetManager();
+        this.tilesetManager.setTilesetFolder(this.assetStructure.getPath(AssetStructure.TILESET));
+        this.tilesetManager.setTerrainFolder(this.assetStructure.getPath(AssetStructure.TERRAIN));
+        this.tilesetManager.setJSONFolder(this.assetStructure.getPath(AssetStructure.JSON));
+
+        this.rootPath = basePath;
     }
 
 }
